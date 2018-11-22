fe5b0dd7ce9450cbbc1687af1bb69e50505b7749
chunyili <chunyili@cw.com.tw>
2018-11-22 10:59:29 +0800

    Initial Commit

---
+++ myMovementView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myMovementView
+//
+//  Created by chunyili on 2018/11/22.
+//  Copyright © 2018 chunyili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
